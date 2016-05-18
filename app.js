1f7d0799a0bedf88f1827cfefdcec4423080a1af
Nikita <nikita.skryt@gmail.com>
2016-05-18 00:54:33 +0000

    code refactoring and optimization

--- app.js
+++ app.js
@@ -1,113 +1,73 @@
 var config = require('./config');
+
 var TelegramBot = require('node-telegram-bot-api');
-var GoogleMapsAPI = require('googlemaps');
 var bot = new TelegramBot(config.get('telegramToken'), {polling: true});
 
-var ForecastIo = require('forecastio');
-var forecastIo = new ForecastIo(config.get('forecastToken'));
+var Weather = require('./lib/weather');
 
-var publicConfig = {
-  key: config.get('googleMapsToken'),
-  stagger_time:       1000, // for elevationPath
-  encode_polylines:   false,
-  secure:             true // use https
-};
-var gmAPI = new GoogleMapsAPI(publicConfig);
+var View = require('./lib/view');
 
+bot.on('message', function(msg, match){
+  Weather.getByQuery(msg.text, function(err, weatherData, geoData) {
+    var message = View.makeMessage(err, weatherData, geoData);
+    if (message) {
+      bot.sendMessage(msg.from.id, message, {
+        parse_mode: "HTML"
+      });
+    }
+   
+  });
+});
 
-bot.on('message', function (msg, match) {
-  var fromId = msg.from.id;
-  
-  
-  if(msg.location) {
-      
-    getCurrentWeather(msg.location.latitude, msg.location.longitude,
-      function(temperature, windSpeed, summary){
-        var message  = 'В этом месте ' + temperature + 
-        ' градусов, ' + summary + ', скорость ветра ' + windSpeed +
-        ' м/с.';
-        bot.sendMessage(fromId, message);
-      }
-    );
-    
-  } else {
-    bot.sendMessage(fromId, 'Отправьте местоположение');
-  }
+bot.on('location', function(msg, match){
+  Weather.getByCoordinates(msg.location.latitude, msg.location.longitude,
+  function(err, weatherData){
+    var message = View.makeMessage(err, weatherData, null);
+    if (message) {
+      bot.sendMessage(msg.from.id, message, {
+        parse_mode: "HTML"
+      });
+    }
+  });
 });
 
 bot.on('inline_query', function(msg, match){
-  if (msg.query == '') {
-    
-    if (msg.location) {
-      getCurrentWeather(msg.location.latitude, msg.location.longitude,
-        function(temperature, windSpeed, summary){
-          var description = temperature + ' °с, ' + summary;
-          var message  = 'Здесь ' + temperature + 
-          ' градусов, ' + summary + ', скорость ветра ' + windSpeed +
-          ' м/с.';
+  if ((msg.query == '') && msg.location) {
+    Weather.getByCoordinates(msg.location.latitude, msg.location.longitude,
+      function(err, weatherData){
+        var inline = View.makeInline(err, weatherData, null);
+        var message = View.makeMessage(err, weatherData, null);
+        if (inline && message) {
           bot.answerInlineQuery(msg.id, [{
             id: msg.id,
             type: "article",
-            description: description,
-            input_message_content: {
-              message_text: message
-              
-            },
-            title: "Ваше местоположение"
-          }]);
-        }
-      );
-    }
-  } else {
-     getCoordinates(msg.query, function(res) {
-       getCurrentWeather(res.geometry.location.lat, res.geometry.location.lng,
-        function(temperature, windSpeed, summary){
-          var description = temperature + ' °с, ' + summary;
-          var message  = '<b>' + res.formatted_address + '<\/b> &#10;' +
-          temperature + 
-          ' градусов, ' + summary + ', скорость ветра ' + windSpeed +
-          ' м/с.';
-          bot.answerInlineQuery(msg.id, [{
-            id: msg.id,
-            type: "article",
-            description: description,
+            description: inline[1],
             input_message_content: {
               message_text: message,
               parse_mode: "HTML"
             },
-            title: res.formatted_address
+            title: inline[0]
           }]);
         }
-      );
-     })
+      });
+    
+  } else {
+    Weather.getByQuery(msg.query, function(err, weatherData, geoData) {
+      var message = View.makeMessage(err, weatherData, geoData);
+      var inline = View.makeInline(err, weatherData, geoData);
+      if (inline && message) {
+        bot.answerInlineQuery(msg.id, [{
+          id: msg.id,
+          type: "article",
+          description: inline[1],
+          input_message_content: {
+            message_text: message,
+            parse_mode: "HTML"
+          },
+          title: inline[0]
+        }]);
+      }
+     
+    });
   }
 });
-
-
-
-function getCurrentWeather(latitude, longitude, callback) {
-  forecastIo.forecast(latitude, longitude, 
-  {
-    units: "si",
-    lang: "ru",
-    exclude: 'minutely, hourly, daily, alerts, flags'
-  }).then(function(data) {
-    
-    
-    
-    callback(data.currently.temperature, data.currently.windSpeed,
-             data.currently.summary);
-  });
-}
-
-function getCoordinates(query, callback) {
-  gmAPI.geocode({
-    "address": query
-  }, function(err, res) {
-    
-    if (!err && res.results[0]) {
-      callback(res.results[0]);
-    } 
-  });
-}
-
